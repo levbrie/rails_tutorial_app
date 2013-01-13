521e7d605da4f14d9efc61306da0015328726408
Lev Brie <lev.brie@gmail.com>
2013-01-12 22:20:31 -0500

    Commit to master of contact page with passing rspec tests and refactoring of static_pages_spec.rb with base_title variable to be used on each to eliminate repetition

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  1et(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
   	# what's in quotes is for humans, irrelevant to RSpec
     it "should have the h1 'Sample App'" do
@@ -21,7 +22,7 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+        :text => "#{base_title} | Home")
     end
   end
 
@@ -34,7 +35,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | Help")
+        :text => "#{base_title} | Help")
     end
   end
 
@@ -47,7 +48,20 @@ describe "Static pages" do
     it "should have the title 'About'" do 
       visit '/static_pages/about'
       page.should have_selector('title', 
-        :text => "Ruby on Rails Tutorial Sample App | About")
+        :text => "#{base_title} | About")
+    end
+  end
+
+  describe "Contact page" do 
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => "Contact")
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', 
+        :text => "#{base_title} | Contact")
     end
   end
 end
