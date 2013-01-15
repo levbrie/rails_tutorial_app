066cca802b0e779c032af86108cd3a3141c601a1
Lev Brie <lev.brie@gmail.com>
2013-01-14 20:29:54 -0500

    Created shared examples for static_pages_spec.rb to further refactor tests, created check that links on layout go to the right pages, and eliminated the need for the full_title test helper by creating spec/helpers/application_helper_spec.rb along with an include for it in spec/support/utilties.rb where the original full_title test helper was

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,57 +4,108 @@ describe "Static pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   subject { page }    # page is the subjects of the tests
+
+  shared_examples_for "all static pages" do 
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
-    before { visit root_path }  # synonymous with before(:each), this creates
-    # a before block to visit root path before each example
-    
-    it { should have_selector('h1', text: 'Sample App') }
-    it {should have_selector 'title', text: full_title('')}
-    it {should_not have_selector 'title', text: '| Home'}
-  	# what's in quotes is for humans, irrelevant to RSpec
-    
-      # Run the generator again with the --webrat flag if you want to use webrat
-      # methods/matchers
-      # original: get static_pages_index_path
-      # response.status.should be(200)
-      # use Capybara function visit to simulate visiting URI /static_pages/home 
-      # in browser
-      # use Capybara page variable to test content in resulting page
-      # remember to use bundle exec with command:
-      # rspec spec/requests/static_pages_spec.rb
-      # to ensure RSpec runs in environment specified by Gemfile
-      # page.should have_selector('h1', :text => 'Sample App')
+    before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    it_should_behave_like "all static pages" 
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
-  describe "Help page" do 
+  describe "Help page" do
     before { visit help_path }
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
   end
 
-  describe "About page" do
+  describe "About page" do 
     before { visit about_path }
-    it { should have_selector('h1', text: 'About')}
-    it { should have_selector('title', text: full_title('About')) }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+    it_should_behave_like "all static pages"
   end
-
-  # old about spec 
-  # describe "About page" do 
-  #   it "should have the h1 'About'" do 
-  #     visit about_path
-  #     page.should have_selector('h1', :text => "About")
-  #   end
-
-  #   it "should have the title 'About'" do 
-  #     visit about_path
-  #     page.should have_selector('title', 
-  #       :text => "#{base_title} | About")
-  #   end
-  # end
 
   describe "Contact page" do 
     before { visit contact_path }
-    it {should have_selector('h1', text: 'Contact')}
-    it {should have_selector('title', text: full_title('Contact'))}
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages" 
   end
-end
+
+  # test for the links on the layout (checks that links go to the right pages)
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact" 
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    # click_link "Sign up now!"
+    # page.should have_selector 'title', text: full_title('Sign up here')
+    # click_link "sample app" 
+    # page.should have_selector 'title', text: full_title('Sample App')
+  end
+end 
+#   describe "Home page" do
+#     before { visit root_path }  # synonymous with before(:each), this creates
+#     # a before block to visit root path before each example
+    
+#     it { should have_selector('h1', text: 'Sample App') }
+#     it {should have_selector 'title', text: full_title('')}
+#     it {should_not have_selector 'title', text: '| Home'}
+#   	# what's in quotes is for humans, irrelevant to RSpec
+    
+#       # Run the generator again with the --webrat flag if you want to use webrat
+#       # methods/matchers
+#       # original: get static_pages_index_path
+#       # response.status.should be(200)
+#       # use Capybara function visit to simulate visiting URI /static_pages/home 
+#       # in browser
+#       # use Capybara page variable to test content in resulting page
+#       # remember to use bundle exec with command:
+#       # rspec spec/requests/static_pages_spec.rb
+#       # to ensure RSpec runs in environment specified by Gemfile
+#       # page.should have_selector('h1', :text => 'Sample App')
+#   end
+
+#   describe "Help page" do 
+#     before { visit help_path }
+#     it { should have_selector('h1', text: 'Help') }
+#     it { should have_selector('title', text: full_title('Help')) }
+#   end
+
+#   describe "About page" do
+#     before { visit about_path }
+#     it { should have_selector('h1', text: 'About')}
+#     it { should have_selector('title', text: full_title('About')) }
+#   end
+
+#   # old about spec 
+#   # describe "About page" do 
+#   #   it "should have the h1 'About'" do 
+#   #     visit about_path
+#   #     page.should have_selector('h1', :text => "About")
+#   #   end
+
+#   #   it "should have the title 'About'" do 
+#   #     visit about_path
+#   #     page.should have_selector('title', 
+#   #       :text => "#{base_title} | About")
+#   #   end
+#   # end
+
+#   describe "Contact page" do 
+#     before { visit contact_path }
+#     it {should have_selector('h1', text: 'Contact')}
+#     it {should have_selector('title', text: full_title('Contact'))}
+#   end
+# end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,15 @@
 # note: files in spec/support directory are automatically included by RSpec
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+
+include ApplicationHelper
+
+# code below no longer needd because of include above and 
+# file spec/helpers/application_helper_spec.rb
+# original full_title creator for application is in app/helpers/application_helper.rb
+# def full_title(page_title)
+# 	base_title = "Ruby on Rails Tutorial Sample App"
+# 	if page_title.empty?
+# 		base_title
+# 	else
+# 		"#{base_title} | #{page_title}"
+# 	end
+# end
