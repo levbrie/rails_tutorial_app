cf46f12c553a65c9a73a569ffdb5efb1ffd85049
Lev Brie <lev.brie@gmail.com>
2013-01-25 19:23:06 -0500

    Completed first and second exercises for chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   end
 
   def create  # RESTful convention - create to complete signin (create session)
-  	user = User.find_by_email(params[:session][:email])
-  	if user && user.authenticate(params[:session][:password])
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
   		# Sign the user in and redirect to the user's show page.
   		sign_in user
   		redirect_to user
@@ -17,6 +17,23 @@ class SessionsController < ApplicationController
   		render 'new'
   	end
   end
+
+
+# old for when we used form_for:
+  # def create  # RESTful convention - create to complete signin (create session)
+  #   user = User.find_by_email(params[:session][:email])
+  #   if user && user.authenticate(params[:session][:password])
+  #     # Sign the user in and redirect to the user's show page.
+  #     sign_in user
+  #     redirect_to user
+  #   else
+  #     # use flash.now to dsiplay flash messages on rendered pages
+  #     # and have them disappear as soon as their is a request (unlike flash)
+  #     flash.now[:error] = 'Invalid email/password combination' 
+  #     render 'new'
+  #   end
+  # end
+
 
   def destroy   #RESTful convention - destroy action to delete sessions (sign out)
     sign_out    # defer to sign_out function for signing out
